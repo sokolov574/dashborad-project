0180a9fddae98e9e17ba46d5a9f3c307439180f2
sokolov574 <sokolovloveoleg@gmail.com>
2024-05-10 14:57:58 +0200

    adding some style for global and for sidebar

---
+++ src/app/dashboard/products/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>ProductsPage</h1>
+    </div>
+  )
+}
+
+export default ProductsPage
---
+++ src/app/dashboard/users/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div>
+      <h1>UsersPage</h1>
+    </div>
+  )
+}
+
+export default UsersPage
