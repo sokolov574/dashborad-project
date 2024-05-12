8c6a421f5c2e9bfe0a7192875f6330c8996b7d49
sokolov574 <sokolovloveoleg@gmail.com>
2024-05-12 13:24:59 +0200

    changed user image and added sidebar style for a user

--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -83,7 +83,11 @@ const Sidebar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.user}>
-        <Image src='/user.png' alt='' width='50' height='50'/>
+        <Image className={styles.userImage} src='/user.png' alt='' width='50' height='50'/>
+        <div className={styles.userDetail}>
+            <span className={styles.username}>John Doe</span>
+            <span className={styles.userTitle}>Administrator</span>
+        </div>
       </div>
       <ul>
         {menuItems.map(cat =>(
