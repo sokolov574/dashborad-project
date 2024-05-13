750d4fa5f7c8022267f4ba8ce77d5fb3c7ef0adb
sokolov574 <sokolovloveoleg@gmail.com>
2024-05-13 21:36:06 +0200

    added some style to the category and all sidebar

--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -89,7 +89,7 @@ const Sidebar = () => {
             <span className={styles.userTitle}>Administrator</span>
         </div>
       </div>
-      <ul>
+      <ul className={styles.list}>
         {menuItems.map(cat =>(
           <li key={cat.title}>
             <span className={styles.cat}>
