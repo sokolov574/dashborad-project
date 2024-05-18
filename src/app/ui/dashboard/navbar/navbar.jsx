1ca9a60ca168241fd0db5eca7c72b84b3f1dd0f8
sokolov574 <sokolovloveoleg@gmail.com>
2024-05-18 23:40:03 +0200

    Added search functionality and new icons to Navbar nd style for title and menu

--- src/app/ui/dashboard/navbar/navbar.jsx
+++ src/app/ui/dashboard/navbar/navbar.jsx
@@ -1,15 +1,32 @@
-'use client'
-
-import { usePathname } from 'next/navigation'
-import styles from './navbar.module.css'
+"use client";
+import { usePathname } from "next/navigation";
+import styles from "./navbar.module.css";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 const Navbar = () => {
-
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
-    <div className={styles.container}></div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search..." className={styles.input} />
+        </div>
+        <div className={styles.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
